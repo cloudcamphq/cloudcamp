b0526271bf13b1b6f1f371a4f77c0f72be85750d
Markus Ecker <markus.ecker@gmail.com>
2021-10-28 18:37:22 +0200

    Initial commit

---
+++ website/src/pages/index.js
@@ -0,0 +1,14 @@
+import * as React from "react";
+import { Link } from "gatsby";
+import Layout from "../components/Layout";
+import { DuplicateIcon } from "@heroicons/react/outline";
+import Header from "../components/Header";
+
+export default function Index({ location }) {
+  return (
+    <div>
+      Work in progress. For now, checkout the{" "}
+      <a href="/docs/tutorial">Tutorial</a>
+    </div>
+  );
+}
