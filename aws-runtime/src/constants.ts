51ea6fb8388f862d5113bbf9c9acd67c1cba0d9d
Markus Ecker <markus.ecker@gmail.com>
2021-12-08 19:09:09 +0100

    docs: document constants

--- aws-runtime/src/constants.ts
+++ aws-runtime/src/constants.ts
@@ -1,15 +1,62 @@
-export const CAMP_HOME_DIR: string = "cloudcamp";
+/**
+ * The default home of CloudCamp in a project.
+ */
+export const CAMP_HOME_DIR = "cloudcamp";
+
+/**
+ * AWS tag - CloudCamp app name
+ */
 export const TAG_APP_NAME = "cloudcamp:app-name";
+
+/**
+ * CDK context key - CloudCamp home. Only passed via command line.
+ */
 export const CONTEXT_KEY_HOME = "@cloudcamp/home";
+
+/**
+ * CDK context key - CloudCamp app name
+ */
 export const CONTEXT_KEY_NAME = "@cloudcamp/name";
+
+/**
+ * CDK context key - source code repository
+ */
 export const CONTEXT_KEY_REPOSITORY = "@cloudcamp/repository";
-export const CONTEXT_KEY_REGION = "@cloudcamp/region";
-export const CONTEXT_KEY_ACCOUNT = "@cloudcamp/account";
+
+/**
+ * CDK context key - source code branch
+ */
 export const CONTEXT_KEY_BRANCH = "@cloudcamp/branch";
+
+/**
+ * CDK context key - AWS region
+ */
+export const CONTEXT_KEY_REGION = "@cloudcamp/region";
+
+/**
+ * CDK context key - AWS account number
+ */
+export const CONTEXT_KEY_ACCOUNT = "@cloudcamp/account";
+
+/**
+ * CDK context key - AWS VPC ID
+ */
 export const CONTEXT_KEY_VPC = "@cloudcamp/vpc";
+
+/**
+ * CDK context key - AWS secret name for repository token
+ */
 export const CONTEXT_REPOSITORY_TOKEN_SECRET =
   "@cloudcamp/repository-token-secret";
+
+/**
+ * CDK context key - CloudCamp version
+ */
 export const CONTEXT_KEY_CLOUDCAMP_VERSION = "@cloudcamp/cloudcamp-version";
+
+/**
+ * CDK context key - cloud service provider
+ */
 export const CONTEXT_KEY_PROVIDER = "@cloudcamp/provider";
 
 export enum CloudCampProvider {
